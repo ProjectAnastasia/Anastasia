8258d8ef9e588f4e3af28ce09880fe235a6adf31
Patrick Meade <blinkdog@protonmail.com>
2025-01-19 18:19:11 -0600

    Add check_grep to Lint job in CI

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 #nb: must be bash to support shopt globstar
@@ -6,31 +6,33 @@ shopt -s globstar
 
 st=0
 
-if grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
+# map checks (*.dmm)
+if grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm; then
     echo "ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!"
     st=1
-fi;
-if grep -P '^\ttag = \"icon' _maps/**/*.dmm;	then
+fi
+if grep -P '^\ttag = \"icon' _maps/**/*.dmm; then
     echo "ERROR: tag vars from icon state generation detected in maps, please remove them."
     st=1
-fi;
-if grep -P 'pixel_[^xy]' _maps/**/*.dmm;	then
-	echo "ERROR: Incorrect pixel offset variables detected in maps, please remove them."
-	st=1
-fi;
-if grep -P 'step_[xy]' _maps/**/*.dmm;	then
+fi
+if grep -P 'pixel_[^xy]' _maps/**/*.dmm; then
+    echo "ERROR: Incorrect pixel offset variables detected in maps, please remove them."
+    st=1
+fi
+if grep -P 'step_[xy]' _maps/**/*.dmm; then
     echo "ERROR: step_x/step_y variables detected in maps, please remove them."
     st=1
-fi;
+fi
 
+# code checks (*.dm)
 if grep -P '^/[\w/]\S+\(.*(var/|, ?var/.*).*\)' code/**/*.dm; then
     echo "ERROR: Changed files contains proc arguments with implicit 'var/', please remove them."
     st=1
-fi;
+fi
 if grep -P '^/*var/' code/**/*.dm; then
     echo "ERROR: Unmanaged global var use detected in code, please use the helpers."
     st=1
-fi;
+fi
 nl='
 '
 nl=$'\n'
@@ -39,7 +41,7 @@ while read f; do
     if [[ ! ${t%x} =~ $r1 ]]; then
         echo "file $f is missing a trailing newline"
         st=1
-    fi;
+    fi
 done < <(find . -type f -name '*.dm')
 
 exit $st

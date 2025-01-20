1cbfbd478235f69626beac4a8ae112de34c5dcf2
Patrick Meade <blinkdog@protonmail.com>
2025-01-19 21:20:27 -0600

    Debugging database test - part 11

--- tools/ci/apply_sql_scripts.sh
+++ tools/ci/apply_sql_scripts.sh
@@ -7,7 +7,7 @@ sql_files=$(find "SQL" -maxdepth 1 -type f -name "*.sql" | sort)
 # apply each SQL file to the database
 for sql_file in $sql_files; do
     echo "Applying: $sql_file"
-    mariadb --host="mariadb" --port="3306" --user="root" --password="root" "anastasia_gamedb" < "$sql_file"
+    mariadb --port="3306" --user="root" --password="root" "anastasia_gamedb" < "$sql_file"
     # failure? log and exit
     if [[ $? -ne 0 ]]; then
         echo "ERROR: Failed to apply $sql_file"
